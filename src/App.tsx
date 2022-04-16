337a28820604676c5b58382909bde87869089749
Antônio Martins <92797981+AntonioCassian@users.noreply.github.com>
2022-04-16 10:02:09 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import TabelaBody from './components/TabelaBody';
+import TabelaFoot from './components/TabelaFoot';
+import TabelaHead from './components/TabelHead';
 
-function App() {
+class App extends Component{
+  state = {
+    livros: []
+  };
+  componentDidMount(){
+    fetch("./api/livros.json")
+    .then(response => response.json())
+    .then(livros => this.setState({livros}))
+    .catch(function(error){
+      console.log("Error na Requisição");
+    })
+    .finally(function(){
+      console.log("Sempre retorna");
+    });
+  };
+  handleRemoverLinha = (id) => {
+    const livros = this.state.livros.filter( l => l.id !== id);
+    this.setState({livros});
+  }
+
+  handleOrdenarCrescente = titulo => {
+    const livros = this.state.livros.sort((a, b) =>
+      a.titulo < b.titulo ? -1 : 0
+    );
+    this.setState({ livros });
+    };
+   handleOrdenarDecrescente = titulo => {
+    const livros = this.state.livros.sort((a, b) =>
+      a.titulo < b.titulo ? -1 : 0
+    );
+    livros.reverse();
+    this.setState({ livros });
+    };
+  
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table className='tabela'>
+      <TabelaHead 
+      ordenarCrescente={ this.handleOrdenarCrescente }
+      ordenarDecrescente={this.handleOrdenarDecrescente}
+      />
+      <TabelaFoot qdeLivros = { this.state.livros.length } /> 
+      <TabelaBody 
+      livros={ this.state.livros } 
+      removerLinha={this.handleRemoverLinha}
+      />
+    </table>
   );
+}
 }
 
 export default App;
